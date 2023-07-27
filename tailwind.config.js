8f93432f23c9b03e92588946aca81cde3d5692f0
Thu Nguyen <nguyenngocanhthu18011994@gmail.com>
2023-07-27 15:38:05 -0400

    second commit more

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/*.{html,js}"],
+  theme: {
+    extend: { },
+    colors: {
+      "dark": "#6F2DBD",
+      "mid" : "#A663CC",
+      "light" : "#B298DC",
+      "blue" :"#8E94F2",
+      "pink": "#DAB6FC",
+      "blue-gray": "#B8D0EB",
+      "teal": "#B9FAF8",
+      "white": "#FFFFFF"
+        }
+  },
+  plugins: [],
+}
+
